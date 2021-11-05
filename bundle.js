1d54e711f344c54e2710d7447c5b3315167f8c24
Douglas <douglaskerins@gmail.com>
2021-11-05 15:05:17 +0000

    adding create note feature

--- bundle.js
+++ bundle.js
@@ -26,4 +26,25 @@
       addNewNote(value, index);
     });
   });
+  var noteButton = document.querySelector("#new-note-button");
+  noteButton.addEventListener("click", () => {
+    let noteTitle = document.querySelector("#new-note-title").value;
+    let noteContent = document.querySelector("#new-note-content").value;
+    let noteIdNumber = document.querySelectorAll(".note").length;
+    let noteObject = {
+      title: noteTitle,
+      content: noteContent
+    };
+    fetch("http://localhost:3000/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(noteObject)
+    }).then((response) => response.json()).then((data) => {
+      console.log("Success:", data);
+    }).then(() => {
+      addNewNote(noteObject, noteIdNumber);
+    });
+  });
 })();
--- index.js
+++ index.js
@@ -7,3 +7,29 @@ fetch('http://localhost:3000/notes')
       addNewNote(value, index);
     })
   });
+
+const noteButton = document.querySelector('#new-note-button');
+noteButton.addEventListener('click', () => {
+  let noteTitle = document.querySelector('#new-note-title').value;
+  let noteContent = document.querySelector('#new-note-content').value;
+  let noteIdNumber = document.querySelectorAll('.note').length;
+  let noteObject = {
+    title: noteTitle,
+    content: noteContent
+  };
+
+  fetch('http://localhost:3000/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(noteObject),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .then(() => {
+      addNewNote(noteObject, noteIdNumber);
+    })
+})
